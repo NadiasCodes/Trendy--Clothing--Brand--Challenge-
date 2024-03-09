7ebaab09a25949f3f2a95b14a38712331b40ea48
NadiasCodes <De.nadia@outlook.com>
2024-03-09 18:40:06 +0100

    added a new section for color palettes and added a third party gallery function

--- java.js
+++ java.js
@@ -36,7 +36,7 @@ ScrollReveal().reveal(".header-title", { delay: 500 });
 ScrollReveal().reveal(".logo, .header-subtitle", { delay: 800 });
 ScrollReveal().reveal(".button-reveal", { delay: 1500, origin: "left" });
 
-ScrollReveal().reveal(".brand-title", { delay: 700, origin: "top" });
-ScrollReveal().reveal(".brand-container", { delay: 900, origin: "left" });
-ScrollReveal().reveal(".gallery-subtitle", { delay: 800, origin: "left" });
-ScrollReveal().reveal(".gallery-box, .swiper", { delay: 600, origin: "left" });
+ScrollReveal().reveal(".brand-title", { delay: 1600, origin: "top" });
+ScrollReveal().reveal(".brand-container", { delay: 1700, origin: "left" });
+ScrollReveal().reveal(".paragraph-subtitle", { delay: 1700, origin: "left" });
+ScrollReveal().reveal(".gallery-box, .swiper", { delay: 800, origin: "left" });
