a6836c8bdfccf3e4f50f88ebd3e8d0e3bde614f0
NadiasCodes <De.nadia@outlook.com>
2024-03-09 12:59:40 +0100

    added @media queries

--- java.js
+++ java.js
@@ -23,3 +23,20 @@ let swiper = new Swiper(".brand-container", {
     },
   },
 });
+
+// scroll reveal
+
+ScrollReveal({
+  distance: "60px",
+  duration: 2500,
+  delay: 400,
+});
+
+ScrollReveal().reveal(".header-title", { delay: 500 });
+ScrollReveal().reveal(".logo, .header-subtitle", { delay: 800 });
+ScrollReveal().reveal(".button-reveal", { delay: 1500, origin: "left" });
+
+ScrollReveal().reveal(".brand-title", { delay: 700, origin: "top" });
+ScrollReveal().reveal(".brand-container", { delay: 900, origin: "left" });
+ScrollReveal().reveal(".gallery-subtitle", { delay: 800, origin: "left" });
+ScrollReveal().reveal(".gallery-box, .swiper", { delay: 600, origin: "left" });
